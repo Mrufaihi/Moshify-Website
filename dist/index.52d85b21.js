42ad7cb512449f5c58b042c0e37a890fe15bd509
Unranked <malrufaihi@gmail.com>
2024-07-05 14:21:00 +0300

    added parcel build and node modules.

---
+++ dist/index.52d85b21.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".collapsible").forEach(e=>e.addEventListener("click",function(){this.classList.toggle("collapsible--extended")}));
+//# sourceMappingURL=index.52d85b21.js.map
